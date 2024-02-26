4c1b996ae308edb0a34947fd403c9a31fec7a463
Wolverlone <gamesmews@hotmail.com>
2024-02-26 13:30:01 +0700

    Complete 8-02

--- WorkshopMultiplayer/Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ WorkshopMultiplayer/Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -10,6 +10,7 @@ using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Text;
 
 public class ClientGameManager
 {
@@ -48,6 +49,15 @@ public class ClientGameManager
 
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
+
+        UserData userData = new UserData
+        {
+            userName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Missing Name")
+        };
+        string payload = JsonUtility.ToJson(userData);
+        byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = payloadBytes;
 
         NetworkManager.Singleton.StartClient();
     }
