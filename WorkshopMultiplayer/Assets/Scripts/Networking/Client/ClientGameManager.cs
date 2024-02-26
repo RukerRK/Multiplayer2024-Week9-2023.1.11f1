193ac4e200e3f36a45446a79ca8ccba6a45b7b87
Wolverlone <gamesmews@hotmail.com>
2024-02-26 14:45:51 +0700

    Complete 8-03

--- WorkshopMultiplayer/Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ WorkshopMultiplayer/Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -11,14 +11,19 @@ using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Text;
+using Unity.Services.Authentication;
 
 public class ClientGameManager
 {
     private JoinAllocation allocation;
+    private NetworkClient networkClient;
+
     private const string MenuSceneName = "Menu";
     public async Task<bool> InitAsync()
     {
-       await UnityServices.InitializeAsync();
+        await UnityServices.InitializeAsync();
+
+        networkClient = new NetworkClient(NetworkManager.Singleton);
 
         AuthState authState = await AuthenticationWrapper.DoAuth();
         if(authState == AuthState.Authenticated)
@@ -52,7 +57,8 @@ public class ClientGameManager
 
         UserData userData = new UserData
         {
-            userName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Missing Name")
+            userName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Missing Name"),
+            userAuthId = AuthenticationService.Instance.PlayerId
         };
         string payload = JsonUtility.ToJson(userData);
         byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
